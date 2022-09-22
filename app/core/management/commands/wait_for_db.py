6b55f011d05c045a7b748f1b9b497f9b623734ed
fromiron <artless1112@gmail.com>
2022-09-22 11:34:19 +0900

    E501 line too long (90 > 79 characters) fixテスト

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -20,6 +20,6 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write('DB unavailable, waiting 1 second...')
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('DB OK'))
+        self.stdout.write('Database available!')
