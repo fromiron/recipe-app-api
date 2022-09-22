e1cd8093e670e587a8de4a50209570cb6c4f09e4
fromiron <artless1112@gmail.com>
2022-09-22 13:56:43 +0900

    add user model

---
+++ app/core/models.py
@@ -0,0 +1,26 @@
+"""
+db models
+"""
+
+from django.db import models
+from django.contrib.auth.models import (
+AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    """manager for users"""
+    def create_user(self, email, password=None, **extra_fields):
+        """create, save and return a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """user in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
