113470a8e9bebef618da755967182c4b77ee2d23
fromiron <artless1112@gmail.com>
2022-09-29 10:48:09 +0900

    urlpatternsの'/'漏れ修正

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-doc'),
     path('api/user/', include('user.urls')),
-    path('api/recipe', include('recipe.urls'))
+    path('api/recipe/', include('recipe.urls'))
 ]
