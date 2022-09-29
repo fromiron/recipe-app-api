78f88452cada64b98b7c1e57313a2fb425ed27e2
fromiron <artless1112@gmail.com>
2022-09-29 09:33:33 +0900

    recipe delete test case 追加

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -103,7 +103,6 @@ class PrivateRecipeApiTests(TestCase):
         res = self.client.post(RECIPES_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         recipe = Recipe.objects.get(id=res.data['id'])
-        print(recipe)
         for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
 
@@ -155,10 +154,10 @@ class PrivateRecipeApiTests(TestCase):
         """test changing the recipe user results in an error"""
         new_user = create_user(email='user2@example.com', password='test123')
         recipe = create_recipe(user=self.user)
-        payload = {'user':new_user.id}
+        payload = {'user': new_user.id}
         url = detail_url(recipe.id)
         self.client.patch(url, payload)
-        
+
         recipe.refresh_from_db()
         self.assertEqual(recipe.user, self.user)
 
@@ -171,3 +170,14 @@ class PrivateRecipeApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Recipe.objects.filter(id=recipe.id).exists())
+
+    def test_recipe_other_users_recipe_error(self):
+        """test trying tp de;ete another users recipe gives error"""
+        new_user = create_user(email='user2@example.com', password='test123')
+        recipe = create_recipe(user=new_user)
+
+        url = detail_url(recipe.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue(Recipe.objects.filter(id=recipe.id).exists())
